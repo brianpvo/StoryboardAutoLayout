2d7e522fa4740cdc9534d3db94a584d9f3294e85
Brian Vo <brianvo@Brians-MacBook-Pro.local>
2018-04-17 14:15:33 -0700

    added purple red, orange, and middle blue box

---
+++ Storyboard AutoLayout/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Storyboard AutoLayout
+//
+//  Created by Brian Vo on 2018-04-17.
+//  Copyright © 2018 Brian Vo. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
